c5a0d1b120e9c20c259d317112579fb127a78d7a
Kian Gorgichuk <kgorgi@users.noreply.github.com>
2016-01-04 23:24:42 -0800

    Update VectorAddition.cpp
    Fixed Whitespace Issue

--- VectorAddition.cpp
+++ VectorAddition.cpp
@@ -1,5 +1,5 @@
 //***********************************************************
-//* VectorAddition.cpp Created By Kian Gorgichuk			*
+//* VectorAddition.cpp Created By Kian Gorgichuk  	     *
 //* Copyright (c) 2015 Kian Gorgichuk. All rights reserved. *
 //***********************************************************
 
